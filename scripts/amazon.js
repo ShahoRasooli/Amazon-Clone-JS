97ceea30c2a4582040bf3585f6c1b15b775f4548
Shaho Rasooli <schaho.rasooli@gmail.com>
2025-12-27 14:41:11 +0000

    use products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    reting: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    image: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    reting: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    reting: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -47,10 +17,10 @@ products.forEach((product) => {
             <div class="product-rating-container">
                 <img
                 class="product-rating-stars"
-                src="images/ratings/rating-${product.reting.stars * 10}.png"
+                src="images/ratings/rating-${product.rating.stars * 10}.png"
                 />
                 <div class="product-rating-count link-primary">${
-                  product.reting.count
+                  product.rating.count
                 }</div>
             </div>
 
@@ -84,7 +54,5 @@ products.forEach((product) => {
             </div>
         `;
 });
-
-console.log(productsHTML);
 
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
