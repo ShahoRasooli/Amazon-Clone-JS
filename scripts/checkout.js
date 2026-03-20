238dccaef917fdced35e2c4e40ee98f48a148c21
ShahoRasooli <schaho.rasooli@gmail.com>
2026-03-20 02:01:01 +0000

    Create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymantSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymantSummary();
