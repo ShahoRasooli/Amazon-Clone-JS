bdc68fd325c6ab1f66f1048a94a20c43b26ebbb4
ShahoRasooli <schaho.rasooli@gmail.com>
2026-03-20 14:38:21 +0000

    Convert products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymantSummary } from "./checkout/paymentSummary.js";
-import '../data/cart-class.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymantSummary();
