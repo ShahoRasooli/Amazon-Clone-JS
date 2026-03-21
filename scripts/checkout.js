098e26fadc4945c3db2f9d8ad326620ee332db4c
ShahoRasooli <schaho.rasooli@gmail.com>
2026-03-21 23:45:41 +0000

    Add backend practice file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymantSummary } from "./checkout/paymentSummary.js";
-//import '../data/cart-class.js';
+//import '../data/cart-class.js'; 
+import '../data/backend-practice.js';
 
 renderOrderSummary();
 renderPaymantSummary();
